118d4bf9b3dacc765e1bb81741ba3ed129d89660
YMDragon <ymdragon@qq.com>
2020-11-19 19:46:55 +0800

    test7

--- nachos/test/test7.c
+++ nachos/test/test7.c
@@ -1,9 +1,10 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int a[20], i;
-    printf("%d\n", a[1000]);
+    printf("%d\n", a[100000]);
     printf("\n");
     return 0;
 }
