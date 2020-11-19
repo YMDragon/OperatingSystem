9afe7916208adbd56aafd5002b75bfe2705c7303
yanQval <yanqval@gmail.com>
2020-11-19 19:36:00 +0800

    tests

---
+++ nachos/test/test6.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[]) {
+    int a,b;
+    a = 10;
+    b = 0;
+    int c;
+    c = a / b;
+
+    printf("%d\n", c);
+
+    return 0;
+}
---
+++ nachos/test/test7.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[]) {
+    int a[20], i;
+    printf("%d\n", a[1000]);
+    printf("\n");
+    return 0;
+}
